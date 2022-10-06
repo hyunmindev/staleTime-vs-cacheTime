b414b7f00cfb2d90dfaa48328fb03f6111cac3f6
hyunmindev <jung@hyunmin.dev>
2022-10-06 16:50:49 +0900

    시나리오1

---
+++ src/hooks/useDetectFetch.js
@@ -0,0 +1,31 @@
+import { useRef, useEffect, useState } from 'react';
+
+export default function useDetectFetch() {
+  const count = useRef(-1);
+  const [state, setState] = useState(false);
+
+  useEffect(() => {
+    const observer = new PerformanceObserver((list) => {
+      // eslint-disable-next-line no-restricted-syntax
+      for (const entry of list.getEntries()) {
+        if (entry.initiatorType === 'fetch') {
+          count.current += 1;
+          if (count.current > 0) {
+            setState(true);
+            count.current = 0;
+          }
+        }
+      }
+    });
+
+    observer.observe({
+      entryTypes: ['resource'],
+    });
+
+    return () => {
+      observer.disconnect();
+    };
+  }, []);
+
+  return state;
+}
